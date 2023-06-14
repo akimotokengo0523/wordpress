a0636e9b1eb04cde656ad97fddf2ca60f2fe7683
akimotokengo0523 <k.akimoto.1987@gmail.com>
2023-06-14 16:27:36 +0000

    init

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import aws_cdk as cdk
 
 from wordpress.wordpress_stack import WordpressStack
 
-my_account = "606417009885"
+my_account = "************"
 my_region = "ap-northeast-1"
 
 envJP = cdk.Environment(account=my_account, region=my_region)
